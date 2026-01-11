a1cad8d2c832b7236699941578d39fcc736fa55c
unknown <742005790@qq.com>
2026-01-11 21:47:14 +0800

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div id="App">hello world</div>
+}
+
+export default App
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"embed"
+
+	"input-stats/app"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := app.NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "Input Stats",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup: app.Startup,
+		Bind: []interface{}{
+			app,
+		},
+		Debug: options.Debug{
+			OpenInspectorOnStartup: true,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
